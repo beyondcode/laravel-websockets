3d89be273c88ef1dd53b61bf7adb08eda8dbfc4e
freek <freek@spatie.be>
2018-11-22 00:10:47 +0100

    nitpicks

--- src/Facades/WebSocketRouter.php
+++ src/Facades/WebSocketRouter.php
@@ -4,16 +4,9 @@ namespace BeyondCode\LaravelWebSockets\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-/**
- * @see \BeyondCode\LaravelWebSockets\Router
- */
+/** @see \BeyondCode\LaravelWebSockets\Router */
 class WebSocketRouter extends Facade
 {
-    /**
-     * Get the registered name of the component.
-     *
-     * @return string
-     */
     protected static function getFacadeAccessor()
     {
         return 'websockets.router';
--- src/LaravelEcho/Http/Controllers/EchoController.php
+++ src/LaravelEcho/Http/Controllers/EchoController.php
@@ -12,34 +12,6 @@ use Symfony\Bridge\PsrHttpMessage\Factory\HttpFoundationFactory;
 
 abstract class EchoController implements HttpServerInterface
 {
-
-    /**
-     * This is called before or after a socket is closed (depends on how it's closed).  SendMessage to $conn will not result in an error if it has already been closed.
-     * @param  ConnectionInterface $conn The socket/connection that is closing/closed
-     * @throws \Exception
-     */
-    function onClose(ConnectionInterface $conn)
-    {
-        //
-    }
-
-    /**
-     * If there is an error with one of the sockets, or somewhere in the application where an Exception is thrown,
-     * the Exception is sent back down the stack, handled by the Server and bubbled back up the application through this method
-     * @param  ConnectionInterface $conn
-     * @param  \Exception $e
-     * @throws \Exception
-     */
-    function onError(ConnectionInterface $conn, \Exception $e)
-    {
-        //
-    }
-
-    /**
-     * @param \Ratchet\ConnectionInterface $conn
-     * @param \Psr\Http\Message\RequestInterface $request null is default because PHP won't let me overload; don't pass null!!!
-     * @throws \UnexpectedValueException if a RequestInterface is not passed
-     */
     public function onOpen(ConnectionInterface $conn, RequestInterface $request = null)
     {
         $queryParameters = [];
@@ -59,15 +31,16 @@ abstract class EchoController implements HttpServerInterface
         $conn->close();
     }
 
-    /**
-     * Triggered when a client sends data through the socket
-     * @param  \Ratchet\ConnectionInterface $from The socket/connection that sent the message to your application
-     * @param  string $msg The message received
-     * @throws \Exception
-     */
     function onMessage(ConnectionInterface $from, $msg)
     {
-        //
+    }
+
+    function onClose(ConnectionInterface $conn)
+    {
+    }
+
+    function onError(ConnectionInterface $conn, \Exception $e)
+    {
     }
 
     abstract public function __invoke(Request $request);
