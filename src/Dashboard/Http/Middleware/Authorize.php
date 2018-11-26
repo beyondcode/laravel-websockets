29866b3e069951814d25abe02e490b83a545c6ff
Marcel Pociot <m.pociot@gmail.com>
2018-11-26 10:04:09 +0100

    wip

--- src/Dashboard/Http/Middleware/Authorize.php
+++ src/Dashboard/Http/Middleware/Authorize.php
@@ -1,10 +1,6 @@
 <?php
 
-<<<<<<< HEAD:src/Dashboard/Http/Middleware/Authorize.php
-namespace BeyondCode\LaravelWebSockets\Dashboard\Http\Middleware;
-=======
 namespace BeyondCode\LaravelWebSockets\Http\Middleware;
->>>>>>> 224845d6fd754bb8f608af61675ef0519c5d2b1d:src/Http/Middleware/Authorize.php
 
 use Illuminate\Support\Facades\Gate;
 
