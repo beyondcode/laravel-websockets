e8e79dfef310fb248b1921d42ee403e1a2dd8019
Marcel Pociot <m.pociot@gmail.com>
2018-11-26 20:55:47 +0100

    wip

--- config/websockets.php
+++ config/websockets.php
@@ -14,9 +14,9 @@ return [
     'clients' => [
         [
             'name' => env('APP_NAME'),
-            'app_id' => env('WEBSOCKETS_APP_ID'),
-            'app_key' => env('WEBSOCKETS_APP_KEY'),
-            'app_secret' => env('WEBSOCKETS_APP_SECRET')
+            'app_id' => env('PUSHER_APP_ID'),
+            'app_key' => env('PUSHER_APP_KEY'),
+            'app_secret' => env('PUSHER_APP_SECRET')
         ],
     ],
 
