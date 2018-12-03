4d549b6610235bd7e030f682484741921830a500
freek <freek@spatie.be>
2018-12-03 15:01:16 +0100

    make colissions less likely

--- src/WebSocketsServiceProvider.php
+++ src/WebSocketsServiceProvider.php
@@ -59,15 +59,15 @@ class WebSocketsServiceProvider extends ServiceProvider
 
     protected function registerRouteMacro()
     {
-        Route::macro('webSockets', function($prefix = 'websockets') {
-            Route::prefix($prefix)->middleware(Authorize::class)->group(function() {
+        Route::macro('webSockets', function($prefix = 'laravel-websockets') {
+            Route::prefix($prefix)->namespace('\\')->middleware(Authorize::class)->group(function() {
                 Route::get('/',  ShowDashboard::class);
                 Route::post('auth', AuthenticateDashboard::class);
                 Route::post('event', SendMessage::class);
             });
 
             //TODO: add middleware
-            Route::prefix($prefix)->group(function() {
+            Route::prefix($prefix)->namespace('\\')->group(function() {
                 Route::post('statistics', [WebsocketStatisticsEntriesController::class, 'store']);
             });
         });
