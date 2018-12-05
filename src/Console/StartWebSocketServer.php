ab0157a826c9078ae4396b072cb305e7715a581e
Marcel Pociot <m.pociot@gmail.com>
2018-12-05 20:10:00 +0000

    Apply fixes from StyleCI

--- src/Console/StartWebSocketServer.php
+++ src/Console/StartWebSocketServer.php
@@ -50,8 +50,8 @@ class StartWebSocketServer extends Command
             'dns' => new DnsResolver(),
             'tls' => [
                 'verify_peer' => config('app.env') === 'production',
-                'verify_peer_name' => config('app.env') === 'production'
-            ]
+                'verify_peer_name' => config('app.env') === 'production',
+            ],
         ]);
 
         $browser = new Browser($this->loop, $connector);
