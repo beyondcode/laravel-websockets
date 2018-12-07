09eb5fb9915be7fb696497bd33d80117a19377ec
Ahmad Fikrizaman Bin Abd Rahim <ahmadfikrizaman@gmail.com>
2018-12-08 00:56:30 +0800

    Reconstruct the PusherBroadcaster

--- src/Dashboard/Http/Controllers/AuthenticateDashboard.php
+++ src/Dashboard/Http/Controllers/AuthenticateDashboard.php
@@ -2,13 +2,27 @@
 
 namespace BeyondCode\LaravelWebSockets\Dashboard\Http\Controllers;
 
-use Illuminate\Http\Request;
+use BeyondCode\LaravelWebSockets\Apps\App;
 use Illuminate\Contracts\Broadcasting\Broadcaster;
+use Illuminate\Http\Request;
+use Pusher\Pusher;
 
 class AuthenticateDashboard
 {
     public function __invoke(Request $request, Broadcaster $broadcaster)
     {
+        /**
+         * Find the app by using the header
+         * and then reconstruct the PusherBroadcaster
+         * using our own app selection. 
+         */
+        $app = App::findById($request->header('x-app-id'));
+
+        $broadcaster->__construct(new Pusher(
+            $app->key, $app->secret,
+            $app->id, $config['options'] ?? []
+        ));
+
         /*
          * Since the dashboard itself is already secured by the
          * Authorize middleware, we can trust all channel
