bc9e0b79536c5699102617e9878e1c7bb87c54fb
Catzilla <11861208+Catzilla@users.noreply.github.com>
2019-03-06 15:28:05 +0200

    Validating a request before filtering

--- src/HttpApi/Controllers/FetchChannelsController.php
+++ src/HttpApi/Controllers/FetchChannelsController.php
@@ -11,14 +11,6 @@ class FetchChannelsController extends Controller
 {
     public function __invoke(Request $request)
     {
-        $channels = Collection::make($this->channelManager->getChannels($request->appId));
-
-        if ($request->has('filter_by_prefix')) {
-            $channels = $channels->filter(function ($channel, $channelName) use ($request) {
-                return Str::startsWith($channelName, $request->filter_by_prefix);
-            });
-        }
-
         $attributes = [];
 
         if ($request->has('info')) {
@@ -27,6 +19,14 @@ class FetchChannelsController extends Controller
             if (in_array('user_count', $attributes) && ! Str::startsWith($request->filter_by_prefix, 'presence-')) {
                 throw new HttpException(400, 'Request must be limited to presence channels in order to fetch user_count');
             }
+        }
+
+        $channels = Collection::make($this->channelManager->getChannels($request->appId));
+
+        if ($request->has('filter_by_prefix')) {
+            $channels = $channels->filter(function ($channel, $channelName) use ($request) {
+                return Str::startsWith($channelName, $request->filter_by_prefix);
+            });
         }
 
         return [
