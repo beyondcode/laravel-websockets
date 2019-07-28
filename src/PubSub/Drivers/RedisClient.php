d43ac821d9f64b6ed4e322849092d0444e821719
anthony <aamining2@gmail.com>
2019-07-28 21:28:35 +0200

    remove test code

--- src/PubSub/Drivers/RedisClient.php
+++ src/PubSub/Drivers/RedisClient.php
@@ -129,7 +129,6 @@ class RedisClient implements ReplicationInterface
      */
     public function subscribe(string $appId, string $channel): bool
     {
-        $this->publishClient->__call('hset', ["$appId:$channel", 541561516, "qsgdqgsd"]);
         if (! isset($this->subscribedChannels["$appId:$channel"])) {
             // We're not subscribed to the channel yet, subscribe and set the count to 1
             $this->subscribeClient->__call('subscribe', ["$appId:$channel"]);
