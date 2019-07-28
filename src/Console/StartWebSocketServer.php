94fb667b90f2791cc419fd2facc8e954baa84a54
Gabriel Iuga <gabriel.iuga90@gmail.com>
2019-07-28 19:18:22 +0100

    Fix for dependency upgrade (#223)
    * + DnsResolver no longer extends React/Dns/Resolver as it's now a final class
    + StartWebSocketServer:getDnsResolver returns ResolverInterface to comply with new react/dns policy
    
    * + organize imports
    
    * + organize imports

--- src/Console/StartWebSocketServer.php
+++ src/Console/StartWebSocketServer.php
@@ -6,9 +6,9 @@ use React\Socket\Connector;
 use Clue\React\Buzz\Browser;
 use Illuminate\Console\Command;
 use React\Dns\Config\Config as DnsConfig;
+use React\Dns\Resolver\ResolverInterface;
 use React\EventLoop\Factory as LoopFactory;
 use React\Dns\Resolver\Factory as DnsFactory;
-use React\Dns\Resolver\Resolver as ReactDnsResolver;
 use BeyondCode\LaravelWebSockets\Statistics\DnsResolver;
 use BeyondCode\LaravelWebSockets\Facades\StatisticsLogger;
 use BeyondCode\LaravelWebSockets\Facades\WebSocketsRouter;
@@ -135,7 +135,7 @@ class StartWebSocketServer extends Command
             ->run();
     }
 
-    protected function getDnsResolver(): ReactDnsResolver
+    protected function getDnsResolver(): ResolverInterface
     {
         if (! config('websockets.statistics.perform_dns_lookup')) {
             return new DnsResolver;
