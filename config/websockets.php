7f8c219b324664bf5dbb39ffc545bbf40219f780
rennokki <rennokki@users.noreply.github.com>
2020-08-13 10:23:13 +0000

    Apply fixes from StyleCI

--- config/websockets.php
+++ config/websockets.php
@@ -132,7 +132,6 @@ return [
          * The Statistics Logger will, by default, handle the incoming statistics, store them
          * and then release them into the database on each interval defined below.
          */
-
         'logger' => \BeyondCode\LaravelWebSockets\Statistics\Logger::class,
 
         /*
