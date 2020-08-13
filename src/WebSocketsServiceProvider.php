a18ef0c6c6f5856f019e7a9f18d36a1e9fa1942e
rennokki <alex@renoki.org>
2020-08-13 19:35:37 +0300

    wip naming

--- src/WebSocketsServiceProvider.php
+++ src/WebSocketsServiceProvider.php
@@ -26,12 +26,12 @@ class WebSocketsServiceProvider extends ServiceProvider
         ], 'config');
 
         $this->publishes([
-            __DIR__.'/../database/migrations/create_websockets_statistics_entries_table.php.stub' => database_path('migrations/'.'000000_create_websockets_statistics_entries_table.php'),
+            __DIR__.'/../database/migrations/create_websockets_statistics_entries_table.php.stub' => database_path('migrations/0000_00_00_000000_create_websockets_statistics_entries_table.php'),
         ], 'migrations');
 
         $this
-        ->registerRoutes()
-        ->registerDashboardGate();
+            ->registerRoutes()
+            ->registerDashboardGate();
 
         $this->loadViewsFrom(__DIR__.'/../resources/views/', 'websockets');
 
