e34c7e8db1a6bccee4ec0b71f3a178de64d46c85
Alex Renoki <rennokki@gmail.com>
2020-08-19 00:02:02 +0300

    Added a redis driver test

---
+++ tests/PubSub/RedisDriverTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace BeyondCode\LaravelWebSockets\Tests\PubSub;
+
+use BeyondCode\LaravelWebSockets\PubSub\Drivers\RedisClient;
+use BeyondCode\LaravelWebSockets\Tests\TestCase;
+use React\EventLoop\Factory as LoopFactory;
+use BeyondCode\LaravelWebSockets\Tests\Mocks\RedisFactory;
+
+class RedisDriverTest extends TestCase
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->runOnlyOnRedisReplication();
+    }
+
+    /** @test */
+    public function redis_listener_responds_properly_on_payload()
+    {
+        $connection = $this->getConnectedWebSocketConnection(['test-channel']);
+
+        $this->pusherServer->onOpen($connection);
+
+        $channelData = [
+            'user_id' => 1,
+            'user_info' => [
+                'name' => 'Marcel',
+            ],
+        ];
+
+        $payload = json_encode([
+            'appId' => '1234',
+            'event' => 'test',
+            'data' => $channelData,
+            'socket' => $connection->socketId,
+        ]);
+
+        $this->getSubscribeClient()->onMessage('1234:test-channel', $payload);
+
+        $this->getSubscribeClient()
+            ->assertEventDispatched('message')
+            ->assertCalledWithArgs('subscribe', ['1234:test-channel'])
+            ->assertCalledWithArgs('onMessage', [
+                '1234:test-channel', $payload,
+            ]);
+    }
+}
