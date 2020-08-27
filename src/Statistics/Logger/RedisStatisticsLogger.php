edcc2f958265f46f23ebc9ee5a296b8059668576
rennokki <alex@renoki.org>
2020-08-27 22:13:50 +0300

    Apply fixes from StyleCI (#491)

--- src/Statistics/Logger/RedisStatisticsLogger.php
+++ src/Statistics/Logger/RedisStatisticsLogger.php
@@ -85,7 +85,6 @@ class RedisStatisticsLogger implements StatisticsLogger
             ? 1
             : max($currentPeakConnectionCount, $currentConnectionCount);
 
-
         $this->redis->hset($this->getHash($appId), 'peak_connection_count', $peakConnectionCount);
     }
 
@@ -105,7 +104,6 @@ class RedisStatisticsLogger implements StatisticsLogger
         $peakConnectionCount = is_null($currentPeakConnectionCount)
             ? 0
             : max($currentPeakConnectionCount, $currentConnectionCount);
-
 
         $this->redis->hset($this->getHash($appId), 'peak_connection_count', $peakConnectionCount);
     }
