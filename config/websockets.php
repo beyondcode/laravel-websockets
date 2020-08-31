0dfa682c97e68ea3d1ecc5c878a6708b731ac3c8
Alex Renoki <rennokki@gmail.com>
2020-08-31 11:06:14 +0300

    Revert "Moved all websockets testing related stuff into a concern"
    This reverts commit aa014add212d23d1440e5bcc15c0d85c719ed94a.

--- config/websockets.php
+++ config/websockets.php
@@ -78,6 +78,7 @@ return [
         [
             'id' => env('PUSHER_APP_ID'),
             'name' => env('APP_NAME'),
+            'host' => env('PUSHER_APP_HOST'),
             'key' => env('PUSHER_APP_KEY'),
             'secret' => env('PUSHER_APP_SECRET'),
             'path' => env('PUSHER_APP_PATH'),
