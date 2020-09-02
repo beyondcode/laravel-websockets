0596d1ad489f1fd9b3decaddda48678efc7d613f
Alex Renoki <rennokki@gmail.com>
2020-09-02 15:06:28 +0300

    Revert "Making channels easily extendable by replacing contents with traits."
    This reverts commit 97e215b68eddc5263af26ccf863bc03cea4eb81d.

--- src/WebSockets/Channels/PrivateChannel.php
+++ src/WebSockets/Channels/PrivateChannel.php
@@ -2,9 +2,25 @@
 
 namespace BeyondCode\LaravelWebSockets\WebSockets\Channels;
 
-use BeyondCode\LaravelWebSockets\Concerns\PrivatelyChannelable;
+use BeyondCode\LaravelWebSockets\WebSockets\Exceptions\InvalidSignature;
+use Ratchet\ConnectionInterface;
+use stdClass;
 
 class PrivateChannel extends Channel
 {
-    use PrivatelyChannelable;
+    /**
+     * Subscribe to the channel.
+     *
+     * @see    https://pusher.com/docs/pusher_protocol#presence-channel-events
+     * @param  \Ratchet\ConnectionInterface  $connection
+     * @param  \stdClass  $payload
+     * @return void
+     * @throws InvalidSignature
+     */
+    public function subscribe(ConnectionInterface $connection, stdClass $payload)
+    {
+        $this->verifySignature($connection, $payload);
+
+        parent::subscribe($connection, $payload);
+    }
 }
