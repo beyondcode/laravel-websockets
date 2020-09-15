6755b42acf3ac2b82a554ad5b92b26a69fc180e2
Alex Renoki <rennokki@gmail.com>
2020-09-15 15:01:10 +0300

    Reverted appId to the payload.

--- src/Dashboard/Http/Controllers/SendMessage.php
+++ src/Dashboard/Http/Controllers/SendMessage.php
@@ -45,9 +45,6 @@ class SendMessage
                 $request->appId
             );
         } else {
-            // Add 'appId' to the payload.
-            $payload['appId'] = $request->appId;
-
             $channelManager->broadcastAcrossServers(
                 $request->appId, $request->channel, (object) $payload
             );
