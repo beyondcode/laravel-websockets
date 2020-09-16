58772324ba016482af16f98a5fe6a8f0173162b6
rennokki <alex@renoki.org>
2020-09-16 08:03:21 +0000

    Apply fixes from StyleCI (#535)

--- src/Channels/PresenceChannel.php
+++ src/Channels/PresenceChannel.php
@@ -127,7 +127,7 @@ class PresenceChannel extends PrivateChannel
                                 $connection->app->id
                             );
                         }
-                     });
+                    });
             });
     }
 }
