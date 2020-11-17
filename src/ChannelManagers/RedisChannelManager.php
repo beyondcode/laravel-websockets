cafd21a0da29c84faa4e208a4c52c54b74678b42
Kristian Løining <kristian@checkin.no>
2020-11-17 13:32:44 +0100

    Fix wrong redis replication connection config path

--- src/ChannelManagers/RedisChannelManager.php
+++ src/ChannelManagers/RedisChannelManager.php
@@ -487,7 +487,7 @@ class RedisChannelManager extends LocalChannelManager
      */
     protected function getConnectionUri()
     {
-        $name = config('websockets.replication.redis.connection', 'default');
+        $name = config('websockets.replication.modes.redis.connection', 'default');
         $config = config("database.redis.{$name}");
 
         $host = $config['host'];
