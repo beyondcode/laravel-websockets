3b0c27c8e3e644151a35a5a1325f04a6662e51a2
myckhel <myckhel1@hotmail.com>
2021-04-20 12:54:31 +0100

    increased redis lock seconds to 2 minutes

--- src/ChannelManagers/RedisChannelManager.php
+++ src/ChannelManagers/RedisChannelManager.php
@@ -809,7 +809,7 @@ class RedisChannelManager extends LocalChannelManager
      */
     protected function lock()
     {
-        return new RedisLock($this->redis, static::$lockName, 0);
+        return new RedisLock($this->redis, static::$lockName, 60 * 2);
     }
 
     /**
