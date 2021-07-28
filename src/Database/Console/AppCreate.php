8a4fb47f0084fc5cf5189731c90c9e5fcf2f894f
Dirk Cziesla <dirk_cziesla@hotmail.de>
2021-07-28 13:03:16 +0100

    add values as options

--- src/Database/Console/AppCreate.php
+++ src/Database/Console/AppCreate.php
@@ -7,13 +7,13 @@ use Illuminate\Console\Command;
 
 class AppCreate extends Command
 {
-    protected $signature = 'websockets:app:create';
+    protected $signature = 'websockets:app:create {--name=} {--host=} {--enable_client_messages=0} {--enable_statistics=0}';
 
     protected $description = 'Create an app and save it on database.';
 
     public function handle()
     {
-        $name = $this->ask('What is the app name? (required)');
+        $name = $this->option('name') || $this->ask('What is the app name? (required)');
 
         if (empty($name)) {
             $this->handle();
@@ -21,11 +21,11 @@ class AppCreate extends Command
             return;
         }
 
-        $host = $this->ask('Host: ');
+        $host = $this->option('host') || $this->ask('Host: ');
 
-        $enable_client_messages = $this->confirm('Would you enable client messages?');
+        $enable_client_messages = $this->option('enable_client_messages') || $this->confirm('Would you enable client messages?');
 
-        $enable_statistics = $this->confirm('Would you enable statistics?');
+        $enable_statistics = $this->option('enable_statistics') || $this->confirm('Would you enable statistics?');
 
         $this->comment('Creating your application, please wait...');
 
