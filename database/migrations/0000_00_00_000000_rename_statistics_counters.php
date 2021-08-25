d56f375c22eebce2548d251fce112fd781652317
Carlos Mora <carlosantoniomora@gmail.com>
2021-08-25 07:56:45 +0200

    Update 0000_00_00_000000_rename_statistics_counters.php
    Renaming columns one by one so it works with SQLite database driver, that cannot handle more than 1 rename/remove column at a time.

--- database/migrations/0000_00_00_000000_rename_statistics_counters.php
+++ database/migrations/0000_00_00_000000_rename_statistics_counters.php
@@ -15,7 +15,11 @@ class RenameStatisticsCounters extends Migration
     {
         Schema::table('websockets_statistics_entries', function (Blueprint $table) {
             $table->renameColumn('peak_connection_count', 'peak_connections_count');
+        });
+        Schema::table('websockets_statistics_entries', function (Blueprint $table) {
             $table->renameColumn('websocket_message_count', 'websocket_messages_count');
+        });
+        Schema::table('websockets_statistics_entries', function (Blueprint $table) {
             $table->renameColumn('api_message_count', 'api_messages_count');
         });
     }
@@ -29,7 +33,11 @@ class RenameStatisticsCounters extends Migration
     {
         Schema::table('websockets_statistics_entries', function (Blueprint $table) {
             $table->renameColumn('peak_connections_count', 'peak_connection_count');
+        });
+        Schema::table('websockets_statistics_entries', function (Blueprint $table) {
             $table->renameColumn('websocket_messages_count', 'websocket_message_count');
+        });
+        Schema::table('websockets_statistics_entries', function (Blueprint $table) {
             $table->renameColumn('api_messages_count', 'api_message_count');
         });
     }
